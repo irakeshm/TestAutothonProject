5ae658c06de8f4f55ce76ac76f0eaa5f4d19a167
Gupta, Richa <Richa.Gupta04@stryker.com>
2019-08-21 12:31:43 +0530

    Added test

--- SeleniumNUnitParallel-master/SeleniumNUnitParallel/UnitTest1.cs
+++ SeleniumNUnitParallel-master/SeleniumNUnitParallel/UnitTest1.cs
@@ -1,5 +1,6 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
+using OpenQA.Selenium.Appium.Android;
 using System;
 
 
@@ -89,15 +90,17 @@ namespace SeleniumNUnitParallel
         }
 
         [Test]
-        public void ChromeGoogleTest()
+        public void ChromeMobileTest()
         {
             try
             {
-                _driver.Navigate().GoToUrl("https://www.google.co.in");
-                _driver.FindElement(By.Name("q")).SendKeys("Selenium");
-                _driver.FindElement(By.Name("btnK")).Click();
-                Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
-                                "Text Selenium was not found");
+                _driver.Navigate().GoToUrl("https://www.youtube.com");
+                 System.Threading.Thread.Sleep(3000);
+                _driver.FindElementByAccessibilityId("Search").Click();
+                //ele.Click();
+                //_driver.FindElement(By.Name("btnK")).Click();
+                //Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                //  "Text Selenium was not found");
 
             }
             catch (Exception e)
