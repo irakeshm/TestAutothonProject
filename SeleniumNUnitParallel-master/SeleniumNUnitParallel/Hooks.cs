e86153200f976fa25274cabbd64d16bc5d5094c4
Rakesh Mishra <irakeshm.tech@gmail.com>
2019-08-21 09:02:31 +0530

    Project Set-up

---
+++ SeleniumNUnitParallel-master/SeleniumNUnitParallel/Hooks.cs
@@ -0,0 +1,57 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Firefox;
+using System;
+using System.Reflection;
+
+namespace SeleniumNUnitParallel
+{
+    [TestFixture]
+    public class Hooks : Base
+    {
+        BrowserType _browser;
+
+        public Hooks(BrowserType browser)
+        {
+            _browser = browser;
+        }
+
+
+        [SetUp]
+        public void SetUpEnvironment()
+        {
+            SelectBrowser(_browser);
+        }
+
+        [TearDown]
+        public void TearDownEnvironment()
+        {
+            Driver.Quit();
+        }
+
+        private void SelectBrowser(BrowserType browser)
+        {
+            switch (browser)
+            {
+                case BrowserType.firefox:
+                    var driverDir = System.IO.Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                    FirefoxDriverService service = FirefoxDriverService.CreateDefaultService(driverDir, "geckodriver.exe");
+                    service.HideCommandPromptWindow = true;
+                    service.SuppressInitialDiagnosticInformation = true;
+                    FirefoxOptions options = new FirefoxOptions();
+                    Driver = new FirefoxDriver(service, options, TimeSpan.FromMinutes(1));
+                    break;
+                case BrowserType.chrome:
+                    Driver = new ChromeDriver();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+    public enum BrowserType
+    {
+        firefox,
+        chrome
+    }
+}
---
+++ SeleniumNUnitParallel-master/SeleniumNUnitParallel/UnitTest1.cs
@@ -0,0 +1,65 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using System;
+
+/// <summary>
+/// Author: Sahil Mutreja
+/// Date: March 24, 2018
+/// Desc: Parallel test execution using NUnit framework and Parallelizable attribute
+/// </summary>
+namespace SeleniumNUnitParallel
+{
+    [TestFixture]
+    [Parallelizable]
+    public class FirefoxTest: Hooks
+    {
+        public FirefoxTest() : base(BrowserType.firefox)
+        {
+        }
+
+        [Test]
+        public void FirefoxGoogleTest()
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl("https://www.google.co.in");
+                Driver.FindElement(By.Name("q")).SendKeys("Selenium");
+                Driver.FindElement(By.Name("btnK")).Click();
+                Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                                "Text Selenium was not found");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } 
+        } 
+    }
+
+    [TestFixture]
+    [Parallelizable]
+    public class ChromeTest : Hooks
+    {
+        public ChromeTest() : base(BrowserType.chrome)
+        {
+        }
+
+        [Test]
+        public void ChromeGoogleTest()
+        {
+            try
+            {
+                Driver.Navigate().GoToUrl("https://www.google.co.in");
+                Driver.FindElement(By.Name("q")).SendKeys("Selenium");
+                Driver.FindElement(By.Name("btnK")).Click();
+                Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                                "Text Selenium was not found");
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            } 
+        }
+    }
+}
