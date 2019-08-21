76a047e6418d3e88e62c9d1ff0bc1e631b86bf78
Gupta, Richa <Richa.Gupta04@stryker.com>
2019-08-21 10:44:20 +0530

    Added Appiumdriver package

--- SeleniumNUnitParallel-master/SeleniumNUnitParallel/Base.cs
+++ SeleniumNUnitParallel-master/SeleniumNUnitParallel/Base.cs
@@ -1,10 +1,16 @@
 ﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
 
 namespace SeleniumNUnitParallel
 {
-    public class 
-        Base
+    public class Base
     {
+        //IWebdriver Instance
         public IWebDriver Driver { get; set; }
+
+
+        //AppiumDriverInstance
+        AppiumDriver<AndroidElement> _driver;
     }
 }
