3c64f1560ec93522bc0fe6a43fbe14af0c377126
Nitin Garg <nitin.garg@stryker.com>
2019-08-21 10:42:47 +0530

    Adding Test case

--- SeleniumNUnitParallel-master/SeleniumNUnitParallel/UnitTest1.cs
+++ SeleniumNUnitParallel-master/SeleniumNUnitParallel/UnitTest1.cs
@@ -14,15 +14,26 @@ namespace SeleniumNUnitParallel
         }
 
         [Test]
-        public void FirefoxGoogleTest()
+        public void LaunchURLandNavigateTOVIdeoTest()
         {
             try
             {
-                Driver.Navigate().GoToUrl("https://www.google.co.in");
-                Driver.FindElement(By.Name("q")).SendKeys("Selenium");
-                Driver.FindElement(By.Name("btnK")).Click();
-                Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
-                                "Text Selenium was not found");
+
+                Driver.Navigate().GoToUrl("https://www.youtube.com");
+                Driver.FindElement(By.XPath("//*[@id='search-icon-legacy']/preceding-sibling::*")).SendKeys("step-inforum");
+                Driver.FindElement(By.XPath("//*[@id='channel-title']/span[text()='STeP-IN Forum'])[1]")).Click();
+                //click Video  tab on step in forum page
+                Driver.FindElement(By.XPath("//*[@id='tabsContent']/paper-tab[2]/div")).Click();
+                //get video name from API
+
+                //Search the video on page
+                string videoName = "";
+
+
+                //Driver.FindElement(By.Name("q")).SendKeys("Selenium");
+                //Driver.FindElement(By.Name("btnK")).Click();
+                //Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
+                //                "Text Selenium was not found");
 
             }
             catch (Exception e)
@@ -31,6 +42,8 @@ namespace SeleniumNUnitParallel
             } 
         } 
     }
+
+    
 
     [TestFixture]
     [Parallelizable]
