9764c95cb547fce9867067f48e9e5050d9225b91
Nitin Garg <nitin.garg@stryker.com>
2019-08-21 12:11:07 +0530

    updating

--- SeleniumNUnitParallel-master/SeleniumNUnitParallel/UnitTest1.cs
+++ SeleniumNUnitParallel-master/SeleniumNUnitParallel/UnitTest1.cs
@@ -1,7 +1,8 @@
 ﻿using NUnit.Framework;
 using OpenQA.Selenium;
 using System;
-
+using System.Collections.Generic;
+using System.Threading;
 
 namespace SeleniumNUnitParallel
 {
@@ -9,7 +10,7 @@ namespace SeleniumNUnitParallel
     [Parallelizable]
     public class FirefoxTest: Hooks
     {
-        public FirefoxTest() : base(BrowserType.firefox)
+        public FirefoxTest() : base(BrowserType.chrome)
         {
         }
 
@@ -20,26 +21,60 @@ namespace SeleniumNUnitParallel
             {
 
                 Driver.Navigate().GoToUrl("https://www.youtube.com");
-                Driver.FindElement(By.XPath("//*[@id='search-icon-legacy']/preceding-sibling::*")).SendKeys("step-inforum");
+                //Driver.FindElement(By.XPath("//*[@id='search-icon-legacy']/preceding-sibling::*")).SendKeys("step-inforum");
+                Driver.FindElement(By.Id("search-form")).Click();
+                String js = "arguments[0].setAttribute('value','nitin')";
+                ((OpenQA.Selenium.IJavaScriptExecutor)Driver).ExecuteScript(js, Driver.FindElement(By.Id("search-form")));
+
+                Driver.FindElement(By.Id("search-form")).SendKeys("step-inforum");
                 Driver.FindElement(By.XPath("//*[@id='channel-title']/span[text()='STeP-IN Forum'])[1]")).Click();
                 //click Video  tab on step in forum page
                 Driver.FindElement(By.XPath("//*[@id='tabsContent']/paper-tab[2]/div")).Click();
                 //get video name from API
 
                 //Search the video on page
-                string videoName = "";
+                string videoName = APICall.fetchAPIResult();
+
+                //locate the video on screen  take screenshot
+                Screenshot ScreenShot = ((ITakesScreenshot)Driver).GetScreenshot();
+                //Save the screenshot
+                String finalpath = @"C:\Users\ngarg1\source\repos\TestAutothonProject2\SeleniumNUnitParallel-master\SeleniumNUnitParallel\bin\Debug\" + "ScreenShot_" + DateTime.Now.ToString("ddMMhhmmss") + ".png";
+                ScreenShot.SaveAsFile(finalpath, ScreenshotImageFormat.Png);
+                //change the video quality to P360
+
+                //Get  the name of all vidoes using Up next
+                IList<IWebElement> nextVideos = Driver.FindElements(By.Id("video-title"));
+                IList<IWebElement> nextVideosDisplayed = new List<IWebElement>();
+                foreach (IWebElement ele in nextVideos)
+                {
+                    if (ele.Displayed)
+                        nextVideosDisplayed.Add(ele);
+                }
+              
+                IList<IWebElement> nextAllVideos = new List<IWebElement>();
+              
+                do
+                {
+                    BrowserSearch.PerformPageLoad(Driver, nextVideosDisplayed);
+                    //((OpenQA.Selenium.IJavaScriptExecutor)Driver).ExecuteScript("arguments[0].scrollIntoView();", nextVideosDisplayed[nextVideosDisplayed.Count - 1]);
+                    //Thread.Sleep(10 * 1000);
+                    nextAllVideos=Driver.FindElements(By.Id("video-title"));
+                    foreach (IWebElement ele in nextAllVideos)
+                    {
+                        if (!ele.Displayed)
+                            nextAllVideos.Remove(ele);
+                    }
+                } while (nextVideosDisplayed.Count != nextAllVideos.Count);
+
+                List<string> NextVideoNames = new List<string>();
+                foreach (IWebElement ele in nextVideosDisplayed)
+                {
+                    if(ele.Displayed)
+                        NextVideoNames.Add(ele.GetAttribute("title"));
+                }
 
 
-                //Driver.FindElement(By.Name("q")).SendKeys("Selenium");
-                //Driver.FindElement(By.Name("btnK")).Click();
-                //Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
-                //                "Text Selenium was not found");
-                APICall.fetchAPIResult();
-                Driver.Navigate().GoToUrl("https://www.google.co.in");
-                Driver.FindElement(By.Name("q")).SendKeys("Selenium");
-                Driver.FindElement(By.Name("btnK")).Click();
-                Assert.That(Driver.PageSource.Contains("Selenium"), Is.EqualTo(true),
-                                "Text Selenium was not found");
+              
 
             }
             catch (Exception e)
@@ -49,7 +84,7 @@ namespace SeleniumNUnitParallel
         } 
     }
 
-    
+   
 
     [TestFixture]
     [Parallelizable]
